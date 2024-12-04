43ffd2a3972d16cedf47e9a762dc5ae29f09603c
Syon Rahman <144486855+SyonRahman@users.noreply.github.com>
2024-12-04 14:20:46 -0500

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day3 {
+    public static void main(String[] args) {
+        File f = new File("src/day3.txt");
+    }
+
+}
